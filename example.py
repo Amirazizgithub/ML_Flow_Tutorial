81b333f1415315d8088e199c070cd8623a1c6821
Amir Aziz <Amira@cmrsl.net>
2025-03-18 22:38:49 +0530

    Elastic_regression_Model

--- example.py
+++ example.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
         mlflow.log_metric("mae", mae)
 
         # For remote server
-        mlflow.set_tracking_uri(os.getenv("MLFLOW_SERVER_URI"))
+        mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
